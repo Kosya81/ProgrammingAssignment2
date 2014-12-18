e545076788fd7826ffccee1cbb9b839476c68fba
Kosya81 <2kosya@gmail.com>
2014-12-18 22:51:15 +0300

    completed PA2

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  im <- NULL
+  set <- function(y) {
+    x <<- y
+    im <<- NULL
+  }
+  get <- function() x
+  setim <- function(inv_matrix) im <<- inv_matrix
+  getim <- function() im
+  list(set = set, get = get,
+       setim = setim,
+       getim = getim)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  im <- x$getim()
+  if(!is.null(im)) {
+    message("getting cashed data")
+    return(im)
+  }
+  data <- x$get()
+  im <- solve(data,...)
+  x$setim(im)
+  im
 }
